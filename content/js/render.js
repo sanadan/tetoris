95156729e6810ea104927ff85b262d82cbb1f2f0
sanadan <jecy00@gmail.com>
2018-01-08 23:27:09 +0900

    仮ブロックで描画するようにした

--- content/js/render.js
+++ content/js/render.js
@@ -5,11 +5,14 @@ var canvas = document.getElementById( "board" )  // キャンバス
 var ctx = canvas.getContext( '2d' ); // コンテクスト
 var W = 200, H = 400;  // キャンバスのサイズ
 var BLOCK_W = W / COLS, BLOCK_H = H / ROWS;  // マスの幅を設定
+var block = new Image() ;
+block.src = "material/block.png" ;
+var BLOCK_SIZE = 32 ;
 
 // x, yの部分へマスを描画する処理
-function drawBlock( x, y ) {
-  ctx.fillRect( BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1 , BLOCK_H - 1 );
-  ctx.strokeRect( BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1 , BLOCK_H - 1 );
+function drawBlock( x, y, c ) {
+  left = ( c - 1 ) * BLOCK_SIZE ;
+  ctx.drawImage( block, left, 0, BLOCK_SIZE, BLOCK_SIZE, BLOCK_W * x, BLOCK_H * y, BLOCK_W, BLOCK_H ) ;
 }
 
 // 盤面と操作ブロックを描画する
@@ -21,8 +24,7 @@ function render() {
   for ( var x = 0; x < COLS; ++x ) {
     for ( var y = 0; y < ROWS; ++y ) {
       if ( board[ y ][ x ] ) {  // マスが空、つまり0ゃなかったら
-        ctx.fillStyle = colors[ board[ y ][ x ] - 1 ];  // マスの種類に合わせて塗りつぶす色を設定
-        drawBlock( x, y );  // マスを描画
+        drawBlock( x, y, board[ y ][ x ] );  // マスを描画
       }
     }
   }
@@ -31,8 +33,7 @@ function render() {
   for ( var y = 0; y < 4; ++y ) {
     for ( var x = 0; x < 4; ++x ) {
       if ( current[ y ][ x ] ) {
-        ctx.fillStyle = colors[ current[ y ][ x ] - 1 ];  // マスの種類に合わせて塗りつぶす色を設定
-        drawBlock( currentX + x, currentY + y );  // マスを描画
+        drawBlock( currentX + x, currentY + y, current[ y ][ x ] );  // マスを描画
       }
     }
   }
@@ -40,3 +41,4 @@ function render() {
 
 // 30ミリ秒ごとに状態を描画する関数を呼び出す
 setInterval( render, 30 );
+
--- content/material/material.md
+++ content/material/material.md
@@ -1,2 +1,4 @@
 # 使用素材
 * [M・S・J 迷彩パターン](http://kasaneteto.jp/twin_down.html)
+* [【亜種っつーかUTAU】重音テトのドット絵【RPGツクール】](http://piapro.jp/t/Ntrw)
+
